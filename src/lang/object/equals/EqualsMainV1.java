114b7277e3062119c971068588a6cdf6dbd43a0c
gpol2300 <gpol123@naver.com>
2024-03-30 18:15:23 +0900

    인프런 자바 중급

--- src/lang/object/equals/EqualsMainV1.java
+++ src/lang/object/equals/EqualsMainV1.java
@@ -6,7 +6,11 @@ public class EqualsMainV1 {
         UserV1 user1 = new UserV1("id-100");
         UserV1 user2 = new UserV1("id-100");
 
-        System.out.println("identity = " + (user1 == user2));
-        System.out.println("equality = " + (user1.equals(user2)));
+        System.out.println("identity = " + (user1 == user2));  //동일성 비교
+        System.out.println("equality = " + (user1.equals(user2)));  //동등성 비교
+
+        //동일성 - 완전히 같은 존재, 물리적으로 같은 메모리에 있는 객체 인스턴스인지 참조값을 확인함
+        //동등성 - 논리적으로 같은 지
+
     }
 }
