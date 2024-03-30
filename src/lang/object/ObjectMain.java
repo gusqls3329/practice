b8081e561349653470159f84fe7e87f0e4cf469f
gpol2300 <gpol123@naver.com>
2024-03-30 16:21:19 +0900

    인프런 자바 중급

--- src/lang/object/ObjectMain.java
+++ src/lang/object/ObjectMain.java
@@ -13,7 +13,15 @@ public class ObjectMain {
 
         // toString()은 Object 클래스의 메서드
         // toString()은 객체에 대한 정보를 제공해줌
-        String string = child.toString();
+        String string = child.toString(); //오브젝트의 toString()을 가져옴
         System.out.println(string);
+
+        /*오브젝트 상속이유
+        1. 공통기능 제공 : 새로운 메소드를 항상 만들어야 하고 같은 기능의 메소드 이름이 다를 수 있다.
+                         공통 기능을 만들어 편리하게 제공(상속) 받을 수 있음
+        2. 다형성의 기본구현 : 오브젝트는 모든 객체를 담을 수 있다.
+                            타입이 다른 객체를 어딘가에 보관해야된다면 오브젝트에 보관
+        */
     }
+
 }
--- src/lang/object/poly/ObjectPolyExample1.java
+++ src/lang/object/poly/ObjectPolyExample1.java
@@ -15,6 +15,8 @@ public class ObjectPolyExample1 {
         //obj.move(); //컴파일 오류, Object는 move()가 없다.
 
         //객체에 맞는 다운캐스팅 필요
+        //instanceof : 객체가 어떤 클래스인지, 어떤 클래스를 상속받았는지 확인하는데 사용하는 연산자 이다.
+        //참조변수가 참조하고 있는 인스턴스의 실제 타입을 알아보기 위해 해당 연산자를 사용한다.
         if (obj instanceof Dog dog) {
             dog.sound();
         } else if (obj instanceof Car car) {
