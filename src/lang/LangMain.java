660e50f37c5526b9f974d9959e9459c5142c5bd0
gpol2300 <gpol123@naver.com>
2024-03-30 15:09:30 +0900

    인프런 자바 중급

--- src/lang/LangMain.java
+++ src/lang/LangMain.java
@@ -1,5 +1,8 @@
 package lang;
-
+//lang 패키지는 자바에서 기본으로 제공하는 라이브러리(클래스모음)
+//자바언어를 이루는 가장 기본적인 클래스를 보관하는 패키지
+//기본으로 임포트 돼있음
+//ex) import java.lang.System; //생략 가능
 public class LangMain {
 
     public static void main(String[] args) {
