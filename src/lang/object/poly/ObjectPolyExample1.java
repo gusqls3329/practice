6440582327f43131e412778531a49cb27aa41a71
gpol2300 <gpol123@naver.com>
2024-03-30 16:21:40 +0900

    인프런 자바 중급

--- src/lang/object/poly/ObjectPolyExample1.java
+++ src/lang/object/poly/ObjectPolyExample1.java
@@ -14,7 +14,7 @@ public class ObjectPolyExample1 {
         //obj.sound(); //컴파일 오류, Object는 sound()가 없다.
         //obj.move(); //컴파일 오류, Object는 move()가 없다.
 
-        //객체에 맞는 다운캐스팅 필요
+        //Object는 자신의 메소드 밖에 모름 - 객체에 맞는 다운캐스팅 필요
         //instanceof : 객체가 어떤 클래스인지, 어떤 클래스를 상속받았는지 확인하는데 사용하는 연산자 이다.
         //참조변수가 참조하고 있는 인스턴스의 실제 타입을 알아보기 위해 해당 연산자를 사용한다.
         if (obj instanceof Dog dog) {
